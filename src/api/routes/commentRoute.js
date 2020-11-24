a98948f786933f2f47c1aebda62263f261bcc67c
Oulimata <oulimataba2005@gmail.com>
2020-11-24 12:19:17 +0100

    mon commentaire cont

--- src/api/routes/commentRoute.js
+++ src/api/routes/commentRoute.js
@@ -1,12 +1,101 @@
-module.exports = (server) => {
-    const commentController = require('../controllers/commentController');
+const Comment = require('../models/commentModel');
+const Post = require('../models/postModel');
 
-    server.route('/posts/:post_id/comments')
-        .get(commentController.list_all_comments)
-        .post(commentController.create_a_comment);
+exports.list_all_comments = (req, res) => {
+    Comment.find({
+        post_id: req.params.post_id
+    }, (error, comments) => {
+        if (error) {
+            res.status(500);
+            console.log(error);
+            res.json({
+                message: "Erreur serveur."
+            })
+        } else {
+            res.status(200);
+            res.json(comments)
+        }
+    })
+}
 
-    server.route('/comments/:comment_id') // req.params.comment_id
-        .get(commentController.get_a_comment)
-        .put(commentController.update_a_comment)
-        .delete(commentController.delete_a_comment);
+exports.create_a_comment = (req, res) => {
+    Post.findById(req.params.post_id, (error, post) => {
+        if (error) {
+            res.status(500);
+            console.log(error);
+            res.json({
+                message: "Erreur serveur."
+            })
+        } else {
+            // req.body.post = req.params.post_id;
+            let new_comment = new Comment({
+                post_id: req.params.post_id,
+                ...req.body
+            });
+            // new_comment.post_id = req.params.post_id;
+
+            new_comment.save((error, comment) => {
+                if (error) {
+                    res.status(500);
+                    console.log(error);
+                    res.json({
+                        message: "Erreur serveur."
+                    })
+                } else {
+                    res.status(201);
+                    res.json(comment)
+                }
+            })
+        }
+    })
+
+}
+
+exports.get_a_comment = (req, res) => {
+    Comment.findById(req.params.comment_id, (error, comment) => {
+        if (error) {
+            res.status(500);
+            console.log(error);
+            res.json({
+                message: "Erreur serveur."
+            })
+        } else {
+            res.status(200);
+            res.json(comment)
+        }
+    })
+}
+
+exports.update_a_comment = (req, res) => {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body, {
+        new: true
+    }, (error, comment) => {
+        if (error) {
+            res.status(500);
+            console.log(error);
+            res.json({
+                message: "Erreur serveur."
+            })
+        } else {
+            res.status(200);
+            res.json(comment)
+        }
+    })
+}
+
+exports.delete_a_comment = (req, res) => {
+    Comment.findByIdAndRemove(req.params.comment_id, (error) => {
+        if (error) {
+            res.status(500);
+            console.log(error);
+            res.json({
+                message: "Erreur serveur."
+            })
+        } else {
+            res.status(200);
+            res.json({
+                message: "Commentaire supprimé !"
+            })
+        }
+    })
 }
